5a20b3cf9a152bc2ea0e8b1e204a9f546ca08f69
Anna Zezulkina <annzez@gmail.com>
2017-10-30 22:32:56 +0300

    add response error handling

--- js/data/loader.js
+++ js/data/loader.js
@@ -2,7 +2,12 @@ const SERVER_URL = `https://es.dump.academy/guess-melody`;
 
 class Loader {
   static loadData() {
-    return fetch(`${SERVER_URL}/questions`).then((res) => res.json());
+    return fetch(`${SERVER_URL}/questions`).then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(`Неизвестный статус: ${response.status} ${response.statusText}`);
+    });
   }
 }
 
